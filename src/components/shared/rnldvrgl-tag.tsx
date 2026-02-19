b32f5a389911fef09e234ca0f31e05aafb7fadcf
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2026-02-19 22:22:26 +0800

    chore: add rnldvrgl tag

---
+++ src/components/shared/rnldvrgl-tag.tsx
@@ -0,0 +1,11 @@
+const RnldvrglTag = () => {
+  return (
+    <p className="text-sm text-code-comment">
+      <span className="text-code-tag">&lt;</span>
+      <span className="text-code-keyword">rnldvrgl</span>
+      <span className="text-code-tag"> /&gt;</span>
+    </p>
+  )
+}
+
+export default RnldvrglTag
